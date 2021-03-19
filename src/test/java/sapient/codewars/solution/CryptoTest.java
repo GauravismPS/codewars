b67dc14564c1284743fb40639035615f905163ad
GauravismPS <root@Zeref.localdomain>
2021-03-19 14:09:52 +0530

    changes to test

--- src/test/java/sapient/codewars/solution/CryptoTest.java
+++ src/test/java/sapient/codewars/solution/CryptoTest.java
@@ -10,8 +10,8 @@ public class CryptoTest {
     public void exampleTests() {
         String[][] tests = {
             {"hello", "0.1.2.2.3"},
-            // {"heLlo", "0.1.2.2.3"},
-            // {"helLo", "0.1.2.2.3"},
+            {"heLlo", "0.1.2.2.3"},
+            {"helLo", "0.1.2.2.3"},
             {"Hippopotomonstrosesquippedaliophobia", "0.1.2.2.3.2.3.4.3.5.3.6.7.4.8.3.7.9.7.10.11.1.2.2.9.12.13.14.1.3.2.0.3.15.1.13"},
 
         };
@@ -22,9 +22,9 @@ public class CryptoTest {
         assertEquals(arr[1], ans);
       }
     }
-    // @Test 
-    // public void shouldAnswerWithTrue()
-    // {
-    //     assertTrue( true );
-    // }
+    @Test 
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
 }
